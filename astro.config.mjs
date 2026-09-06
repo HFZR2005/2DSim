7b3f2dda093dbef359ef3c2555c4d8514c07def4
Hifzur Rahman <hr493@cam.ac.uk>
2026-09-06 12:43:59 +0100

    site map

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 // @ts-check
+import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mechanicslab.org',
+  integrations: [
+    sitemap({
+      filter: (page) => !page.includes('/404'),
+    }),
+  ],
 });
